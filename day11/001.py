50c9b68d73b14e486ed0f557ee35773c3384b4f0
JaehyuAN <jat0112@naver.com>
2024-01-12 15:30:41 +0900

    홈페이지 1차완성

---
+++ day11/001.py
@@ -0,0 +1,12 @@
+class jumsu:
+    name=input('이름을 입력해주세요')
+    kor=int(input('국어점수를 입력해주세요.'))
+    eng=int(input('영어점수를 입력해주세요.'))
+    mat=int(input('수학점수를 입력해주세요.'))
+    def hap():
+        hap=
+
+
+
+    
+     
